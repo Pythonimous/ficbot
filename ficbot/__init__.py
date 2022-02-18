13a8edaa8b6766dc12020775b728c2028b1342f4
Kirill Nikolaev <kir.nikolaev.7@gmail.com>
2022-02-18 21:19:12 +0700

    primitive test and get_links encapsulation

---
+++ ficbot/__init__.py
@@ -0,0 +1,1 @@
+from .data import get_char_links, get_char_pages
---
+++ tests/test_data.py
@@ -0,0 +1,27 @@
+from .context import ficbot
+
+import unittest
+
+
+class DownloadTestCase(unittest.TestCase):
+    """ Tests for link and character page download functions """
+    def setUp(self):
+        """
+        Character descriptions which should be beautified
+        """
+        self.descriptions_dirty = [
+            "Here's a description.\n(Source: Somewebsite)",
+            "\n\nHere's another description.\n\nHere's one more.",
+            "       Here's the last description.\n\n           ",
+            "No biography written."
+        ]
+        self.descriptions_clean = [
+            "Here's a description.",
+            "Here's another description.\nHere's one more.",
+            "Here's the last description.",
+            ""
+        ]
+
+    def test_beautify(self):
+        descriptions_beautified = [ficbot.data.get_char_pages.beautify_bio(bio) for bio in self.descriptions_dirty]
+        self.assertEqual(descriptions_beautified, self.descriptions_clean, 'Bios not beautified as expected.')
