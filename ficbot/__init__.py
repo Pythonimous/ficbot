ce2234980e045b45c2f57fcbbe94621e08312f80
Kirill Nikolaev <kir.nikolaev.7@gmail.com>
2022-02-22 22:48:01 +0700

    added rows to init

--- ficbot/__init__.py
+++ ficbot/__init__.py
@@ -1,3 +1,4 @@
-from .data import download_data
+from .data import download_data, preprocess_data
 from .data.loaders import tf_loaders
 from .features import vectorizer
+from .models.tf import model_name, predict_name, train_name
