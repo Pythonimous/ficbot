77efbcead18be3e7ab764e9e554062a7199e6ce3
Kirill Nikolaev <kir.nikolaev.7@gmail.com>
2022-02-23 10:00:17 +0700

    simple lstm -> gru

--- ficbot/models/tf/model_name.py
+++ ficbot/models/tf/model_name.py
@@ -1,14 +1,13 @@
 import tensorflow as tf
 from tensorflow.keras.applications.vgg16 import VGG16
-from tensorflow.keras.applications.vgg16 import preprocess_input
 
 
 def simple_image_name_model(maxlen, vocab_size, *, loss, optimizer, colab_tpu=False):
     image_input = tf.keras.Input(shape=(224, 224, 3), name="IMAGE_INPUT")
     image_features = VGG16(weights="imagenet", include_top=False, pooling='avg')(image_input)
     name_input = tf.keras.Input(shape=(maxlen, vocab_size), name="NAME_INPUT")
-    lstm = tf.keras.layers.LSTM(128)(name_input)
-    concatenated_features = tf.keras.layers.Concatenate(axis=1)([image_features, lstm])
+    gru = tf.keras.layers.GRU(256)(name_input)
+    concatenated_features = tf.keras.layers.Concatenate(axis=1)([image_features, gru])
     output = tf.keras.layers.Dense(vocab_size, activation="softmax")(concatenated_features)
     model = tf.keras.Model(inputs=[image_input, name_input], outputs=output, name="SimpleImageNameModel")
     model.compile(loss=loss, optimizer=optimizer)
